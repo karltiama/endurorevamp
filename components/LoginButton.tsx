4b799c8ace9304874680ae221669e76fd73d05e4
Karl Tiama <karltiama@gmail.com>
2025-05-29 23:05:25 -0400

    Feat: implement LoginButton component with Strava authentication

---
+++ components/LoginButton.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
+import { getStravaAuthUrl } from "@/lib/strava"
+
+export function LoginButton() {
+
+
+  return (
+    <Button asChild>
+      <Link 
+        href={getStravaAuthUrl()}
+      >
+        Login with Strava
+      </Link>
+    </Button>
+  )
+}
